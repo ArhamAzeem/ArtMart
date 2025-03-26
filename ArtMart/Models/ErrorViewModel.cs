71e56cb255118fa183edeff7c0651fdfc7438b17
ArhamAzeem <164183994+ArhamAzeem@users.noreply.github.com>
2025-03-26 08:56:19 +0500

    Add project files.

---
+++ ArtMart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
